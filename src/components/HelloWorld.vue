4817001f1ff19c28d2c50fc4930b858f4ae7c326
AstroSyntax <smcsorl@gmail.com>
2021-03-18 01:50:05 +0000

    x3000xx

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -29,7 +29,7 @@
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator'
 import io from 'socket.io-client'
-const socket = io.io('https://thestox.co.uk:3000')
+const socket = io.io('https://localhost:3000')
 
 @Component({
   components: {
